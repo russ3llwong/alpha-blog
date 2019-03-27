8c611d2adab2032c201a2ab2c1e9d7cc064f873a
russ3llwong <russellwong97@hotmail.com>
2019-03-27 11:43:57 -0700

    fixed root

---
+++ app/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController #extends ApplicationController
+
+	def home #home action
+
+	end
+
+	def about #about action
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #creates 2 routes
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
-  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
 end
